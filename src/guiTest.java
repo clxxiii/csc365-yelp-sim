c8f27421be09f6e39820979d7164febbd80e9243
Henry <henryjrose2@gmail.com>
2023-02-07 16:45:37 -0500

    init

---
+++ src/guiTest.java
@@ -0,0 +1,44 @@
+import java.awt.Dimension;
+import java.awt.Point;
+
+import javax.swing.*;
+
+
+public class guiTest {
+
+	public static void main(String[] args) {
+		
+		String[] listOfMovies = {
+				"Batman",
+				"Batman 2",
+				"Batman 3"
+		};
+		
+		
+		
+		
+		JFrame frame = new JFrame("HelloWorldSwing");
+	    frame.setPreferredSize(new Dimension(500, 500));
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocation(new Point(500, 150));
+        
+        
+        JLabel label = new JLabel("Please select a movie!");
+        
+        
+        JComboBox movieList = new JComboBox(listOfMovies);
+        movieList.addActionListener(movieList);
+        
+        frame.getContentPane().add(label);
+        frame.getContentPane().add(movieList);
+        
+ 
+        //Display the window.
+        
+        
+        
+        frame.pack();
+        frame.setVisible(true);
+	}
+
+}
