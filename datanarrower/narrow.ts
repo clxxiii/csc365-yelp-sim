d1067c890d6bc22099ab5430f9f68d58949b9bc4
clxxiii <clxxiii@users.noreply.github.com>
2023-02-20 20:22:58 -0500

    add simple data managing stuff

---
+++ datanarrower/narrow.ts
@@ -0,0 +1,23 @@
+import { readFileSync, writeFileSync } from "fs";
+
+const data = readFileSync("./data/yelp_academic_dataset_business.json")
+  .toString()
+  .split("\n");
+
+const set = [];
+let i = 0;
+for (const line of data) {
+  const json: any = JSON.parse(line);
+
+  if (
+    json.categories != null &&
+    json.categories.split(", ").includes("Restaurants")
+  ) {
+    set.push(json);
+    i++;
+  }
+
+  if (i >= 10000) break;
+}
+
+writeFileSync("./data/dataset.json", JSON.stringify(set));
