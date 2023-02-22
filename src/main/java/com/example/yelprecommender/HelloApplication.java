eedbf7ac5029491e6156944d987948d8f71dba01
clxxiii <clxxiii@users.noreply.github.com>
2023-02-21 19:14:17 -0500

    add barebones springboot application

--- src/main/java/com/example/yelprecommender/HelloApplication.java
+++ src/main/java/com/example/yelprecommender/HelloApplication.java
@@ -1,13 +1,22 @@
 package main.java.com.example.yelprecommender;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloApplication {
 
-  @GetMapping("/")
-  public String index() {
-    return "Hello World!";
+  @GetMapping("/get_names")
+  public String[] listBusinessNames() {
+    String[] list = { "Hello World!" };
+    return list;
+  }
+
+  @GetMapping("/recommend")
+  public String recommendBusinees(
+    @RequestParam(value = "id", defaultValue = "0") String id
+  ) {
+    return "Fetching two businesses related to Business ID " + id;
   }
 }
