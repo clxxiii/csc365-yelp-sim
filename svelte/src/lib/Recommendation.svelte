15f7c7f8ae05e981ced37cbc4a0f4f2cb0d380ef
clxxiii <clxxiii@users.noreply.github.com>
2023-02-28 12:10:00 -0500

    Better Recommendation card src

--- svelte/src/lib/Recommendation.svelte
+++ svelte/src/lib/Recommendation.svelte
@@ -1,21 +1,56 @@
 <script lang="ts">
+    import Stars from "./Stars.svelte";
+
   export let text: string;
+
+  const data = JSON.parse(text)
+  console.log(data);
 </script>
 
 <div class="recommendation">
-  <div class="text">{text}</div>
+  <div class="name">{data.name}</div>
+  <div class="location">{data.city}, {data.state}</div>
+  <div class="tags">
+    <b>Tags:</b> {data.categories}
+  </div>
+  <div class="stars"><Stars rating={data.stars} /></div>
 </div>
 
 <style>
   .recommendation {
+    position: relative;
     transition: 0.3s;
     width: 40%;
     height: 150px;
     margin: 10px;
     padding: 5px;
-    background-color: #fff7;
-    backdrop-filter: blur(5px);
+    background-color: #5557;
+    color: white;
     border-radius: 10px;
+  }
+  .stars {
+    position: absolute;
+    top: 5px;
+    left: 5px;
+  }
+  .name {
+    padding-top: 20px;
+    font-size: 1.5em;
+  }
+  .location {
+    position: absolute;
+    top: 5px;
+    right: 5px;
+  }
+  .tags {
+    position: absolute;
+    width: 100%;
+    padding: 5px;
+    bottom: 0;
+    left: 0;
+    overflow: hidden;
+    white-space: nowrap;
+    text-overflow: ellipsis;
   }
 
   .recommendation:hover {
---
+++ svelte/src/lib/Stars.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import Star from "./Star.svelte";
+
+  export let rating: number = 5;
+  console.log(rating);
+
+  type starType = "fill" | "half" | "empty";
+
+  let ratingNumber = rating;
+  const starArray: starType[] = [];
+  while (ratingNumber > 0) {
+    if (ratingNumber >= 1) { 
+      starArray.push("fill");
+      ratingNumber -= 1;
+      continue;
+    }
+
+    if (rating >= 0.5) {
+      starArray.push("half")
+      ratingNumber -= 0.5;
+      continue
+    } 
+  }
+  while (starArray.length < 5) {
+    starArray.push('empty')
+  }
+
+</script>
+
+{#each starArray as star}
+  <Star type={star} />
+{/each}
+
+<style>
+</style>
