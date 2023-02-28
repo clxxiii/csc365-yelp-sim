ff59b68e5a73320a9ad403c67b14938e14d1b731
clxxiii <clxxiii@users.noreply.github.com>
2023-02-28 12:13:37 -0500

    include IDs in get_names request

--- src/main/java/com/example/yelprecommender/HelloApplication.java
+++ src/main/java/com/example/yelprecommender/HelloApplication.java
@@ -1,49 +1,60 @@
 package main.java.com.example.yelprecommender;
-import main.java.functions.locality;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.util.Iterator;
-import java.util.Map;
-import java.util.Scanner;
 
 import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
 import java.math.*;
 import java.nio.Buffer;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.InputMismatchException;
-import java.util.ArrayList;
-
-
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Scanner;
+import main.java.functions.locality;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloApplication {
 
   @GetMapping("/get_names")
-  public ArrayList<String> listBusinessNames() throws Exception {
+  public ArrayList<ArrayList<String>> listBusinessNames() throws Exception {
     // This is just a placeholder to test the frontend
     String path = "data/smallerListOfRestaurants.txt";
     BufferedReader reader = new BufferedReader(new FileReader(path));
     String Line = reader.readLine();
 
+    ArrayList<String> ids = new ArrayList<String>();
+    ArrayList<String> names = new ArrayList<String>();
+    ArrayList<ArrayList<String>> listOfRestraunts = new ArrayList<ArrayList<String>>();
 
-    ArrayList<String> listOfRestraunts = new ArrayList<String>();
-        while(Line != null){
-            listOfRestraunts.add(locality.getName(Line));
-            Line = reader.readLine();
-        }
+    while (Line != null) {
+      names.add(locality.getName(Line));
+      ids.add(locality.getID(Line));
+      Line = reader.readLine();
+    }
 
+    listOfRestraunts.add(ids);
+    listOfRestraunts.add(names);
+
+    reader.close();
     return listOfRestraunts;
   }
 
   @GetMapping("/recommend")
-  public String recommendBusinees(@RequestParam(value = "id",
-                                                defaultValue = "0") String id) {
-    return "Fetching two businesses related to Business ID " + id;
+  public ArrayList<String> recommendBusinees(
+    @RequestParam(value = "id", defaultValue = "0") String id
+  ) {
+    // Run prediction algorithm...
+
+    ArrayList<String> response = new ArrayList<String>();
+    response.add("Business 1");
+    response.add("Business 2");
+    return response;
   }
 }
